2eac343128cafa697430365c33e9a4befa62e319
ncocana <ncocana@cifpfbmoll.eu>
2023-03-31 18:02:48 +0200

    feat(App, README, pom): add database connection pools

---
+++ README.md
@@ -0,0 +1,3 @@
+# MariaDB JDBC
+
+Link to the tutorial: [How to Connect Java Applications to MariaDB Using JDBC](https://mariadb.com/resources/blog/how-to-connect-java-applications-to-mariadb-using-jdbc/).
--- src/main/java/edu/craptocraft/jdbc/App.java
+++ src/main/java/edu/craptocraft/jdbc/App.java
@@ -6,13 +6,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 public class App {
 
-    private static Connection connection;
+    private static HikariDataSource dataSource;
 
     public static void main( String[] args ) throws SQLException{
         try {
-            openDatabaseConnection();
+            initDatabaseConnectionPool();
             deleteData("%");
             createData("Java", 10);
             createData("JavaScript", 9);
@@ -23,35 +25,50 @@ public class App {
             deleteData("C++");
             readData();
         } finally {
-            closeDatabaseConnection();
+            closeDatabaseConnectionPool();
         }
+    }
+
+    private static void initDatabaseConnectionPool() {
+        dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mariadb://localhost:3306/jdbc_demo");
+        dataSource.setUsername("ncocana");
+        dataSource.setPassword("password");
+    }
+
+    private static void closeDatabaseConnectionPool() {
+        dataSource.close();
     }
 
     private static void createData(String name, int rating) throws SQLException {
         System.out.println("Creating data....");
         int rowsInserted;
-        try (PreparedStatement statement = connection.prepareStatement("INSERT INTO programming_language(name, rating) VALUES (?, ?)")) {
-            statement.setString(1, name);
-            statement.setInt(2, rating);
-            rowsInserted = statement.executeUpdate();
-            statement.close();
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO programming_language(name, rating) VALUES (?, ?)")) {
+                statement.setString(1, name);
+                statement.setInt(2, rating);
+                rowsInserted = statement.executeUpdate();
+                statement.close();
+            }
         }
         System.out.println("Rows inserted: " + rowsInserted);
     }
 
     private static void readData() throws SQLException {
         System.out.println("Reading data....");
-        try (PreparedStatement statement = connection.prepareStatement("SELECT name, rating FROM programming_language ORDER BY rating DESC")) {
-            ResultSet resultSet = statement.executeQuery();
-            boolean empty = true;
-            while (resultSet.next()) {
-                String name = resultSet.getString(1);
-                int rating = resultSet.getInt(2);
-                System.out.println("\t" + name + ": " + rating);
-                empty = false;
-            }
-            if (empty) {
-                System.out.println("\t (no data)");
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("SELECT name, rating FROM programming_language ORDER BY rating DESC")) {
+                ResultSet resultSet = statement.executeQuery();
+                boolean empty = true;
+                while (resultSet.next()) {
+                    String name = resultSet.getString(1);
+                    int rating = resultSet.getInt(2);
+                    System.out.println("\t" + name + ": " + rating);
+                    empty = false;
+                }
+                if (empty) {
+                    System.out.println("\t (no data)");
+                }
             }
         }
     }
@@ -59,11 +76,13 @@ public class App {
     private static void updateData(String name, int newRating) throws SQLException {
         System.out.println("Updating data....");
         int rowsUpdated;
-        try (PreparedStatement statement = connection.prepareStatement("UPDATE programming_language SET rating = ? WHERE name LIKE ?")) {
-            statement.setInt(1, newRating);
-            statement.setString(2, name);
-            rowsUpdated = statement.executeUpdate();
-            statement.close();
+        try (Connection connection = dataSource.getConnection()) {
+                try (PreparedStatement statement = connection.prepareStatement("UPDATE programming_language SET rating = ? WHERE name LIKE ?")) {
+                statement.setInt(1, newRating);
+                statement.setString(2, name);
+                rowsUpdated = statement.executeUpdate();
+                statement.close();
+            }
         }
         System.out.println("Rows updated: " + rowsUpdated);
     }
@@ -71,28 +90,13 @@ public class App {
     private static void deleteData(String name) throws SQLException {
         System.out.println("Deleting data....");
         int rowsDeleted;
-        try (PreparedStatement statement = connection.prepareStatement("DELETE FROM programming_language WHERE name LIKE ?")) {
-            statement.setString(1, name);
-            rowsDeleted = statement.executeUpdate();
-            System.out.println("Rows deleted: " + rowsDeleted);
-            statement.close();
+        try (Connection connection = dataSource.getConnection()) {
+                try (PreparedStatement statement = connection.prepareStatement("DELETE FROM programming_language WHERE name LIKE ?")) {
+                statement.setString(1, name);
+                rowsDeleted = statement.executeUpdate();
+                System.out.println("Rows deleted: " + rowsDeleted);
+                statement.close();
+            }
         }
-    }
-
-    private static void openDatabaseConnection() throws SQLException {
-        System.out.println("Connecting to the database....");
-        connection = DriverManager.getConnection(
-            "jdbc:mariadb://localhost:3306/jdbc_demo",
-            "ncocana",
-            "password"
-        );
-        // if (true) throw new RuntimeException("Simulated error!");
-        System.out.println("Connection valid: " + connection.isValid(5));
-    }
-
-    private static void closeDatabaseConnection() throws SQLException {
-        System.out.println("Closing database connection....");
-        connection.close();
-        System.out.println("Connection valid: " + connection.isValid(5));
     }
 }
